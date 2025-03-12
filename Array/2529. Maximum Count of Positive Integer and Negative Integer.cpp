ea49c85731961058e73d0ff5debe8c6819524d49
akashdas95 <dasakind1@gmail.com>
2025-03-12 17:12:44 +0600

    leetcode POTD

---
+++ Array/2529. Maximum Count of Positive Integer and Negative Integer.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int maximumCount(vector<int> &nums)
+    {
+        int positiveCount = 0;
+        int negativeCount = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] < 0)
+            {
+                negativeCount++;
+            }
+            if (nums[i] > 0)
+            {
+                positiveCount++;
+            }
+        }
+        return max(negativeCount, positiveCount);
+    }
+};
